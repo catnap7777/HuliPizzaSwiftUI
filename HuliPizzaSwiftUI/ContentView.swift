14dce55f5de21276e47c848a8c3495de2718147b
km <35552602+catnap7777@users.noreply.github.com>
2022-03-01 17:28:13 -0600

    Initial Commit

---
+++ HuliPizzaSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizzaSwiftUI
+//
+//  Created by Karen Mathes on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HuliPizzaSwiftUI/HuliPizzaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaSwiftUIApp.swift
+//  HuliPizzaSwiftUI
+//
+//  Created by Karen Mathes on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
