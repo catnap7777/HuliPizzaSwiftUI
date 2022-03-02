9b9753f7e62858d05eab02dad76497eb581a2592
km <35552602+catnap7777@users.noreply.github.com>
2022-03-02 11:06:41 -0600

    initial commit for lesson 2

--- HuliPizzaSwiftUI/ContentView.swift
+++ HuliPizzaSwiftUI/ContentView.swift
@@ -10,17 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Huli Pizza Company")
-                .font(.title)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.green)
-            Image("Surf Board")
-                .resizable()
-                .scaledToFit()
+            //.. order of how you put things in Zstack matters; if for example, you listed Text first and Image second, the image will overlay the Text. So, you have to do it the other way here to have text on top of image
+            ZStack {
+                Image("Surf Board")
+                    .resizable()
+                    .scaledToFit()
+                Text("Huli Pizza Company")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.green)
+            }
+            //.. used to be this way without ZStack
+//            Text("Huli Pizza Company")
+//                .font(.title)
+//                .fontWeight(.heavy)
+//                .foregroundColor(Color.green)
+//            Image("Surf Board")
+//                .resizable()
+//                .scaledToFit()
             Text("Order Pizza")
                 .font(.largeTitle)
-            //.. spacers push everything away from the closest margin or object; since this is at bottom of vstack, it pushes whole thing up toward top
+            //.. spacers push everything away from the closest margin or object; since this is at bottom of VStack, it pushes whole thing up toward top
             Spacer()
+            //.. can use alignment to move text, etc.
+            HStack(alignment: .top, spacing: 15) {
+                Image("1_100w")
+                Text("Huli Chicken Pizza")
+                //.. to left justify HStack
+                Spacer()
+            }
+            Text("Your Order")
+            //.. can use alignment to move text, etc.
+            HStack(alignment: .firstTextBaseline) {
+                Text("Your order item here:")
+                Spacer()
+                Text("$0.00")
+            }
         }
         .padding()
     }
@@ -29,17 +54,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
+            //.. to have multiple devices previewed, use Group and put another ContentView
+            ContentView()
+                .previewDevice("iPhone 12 Pro")
+                .previewDisplayName("iPhone 12 Pro")
+            
             ContentView()
                 //.. for dark mode
                 .colorScheme(.dark)
                 .background(Color.black)
             //.. for specific device preview
-                .previewDevice("iPad Pro (9.7-inch)")
-                .previewDisplayName("iPad Pro 9.7 inch")
-            //.. to have multiple devices previewed, use Group and put another ContentView
-            ContentView()
-                .previewDevice("iPhone 12 Pro")
-                .previewDisplayName("Karen's iPhone 12 Pro")
+//                .previewDevice("iPad Pro (9.7-inch)")
+//                .previewDisplayName("iPad Pro 9.7 inch")
+                .previewDevice("iPhone 11")
+                .previewDisplayName("iPhone 11 Dark Mode")
+            
         }
     }
 }
