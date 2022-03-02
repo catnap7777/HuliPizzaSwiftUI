e2520b11eb0029da4e28d1387016202242b2cf6f
km <35552602+catnap7777@users.noreply.github.com>
2022-03-02 13:26:36 -0600

    added space modifiers

--- HuliPizzaSwiftUI/OrderView/OrderListView.swift
+++ HuliPizzaSwiftUI/OrderView/OrderListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct OrderListView: View {
     var body: some View {
         VStack {
-            Text("Your Order")
+            ListHeaderView(text: "Your Order")
             //.. can use alignment to move text, etc.
             List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
                 OrderRowView()
