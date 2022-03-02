9fd9c0e89b72bd0dac62d8284bb43a1149c6b697
km <35552602+catnap7777@users.noreply.github.com>
2022-03-02 10:00:28 -0600

    more

--- HuliPizzaSwiftUI/ContentView.swift
+++ HuliPizzaSwiftUI/ContentView.swift
@@ -11,19 +11,35 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Huli Pizza Company")
-                .font(.largeTitle)
+                .font(.title)
                 .fontWeight(.heavy)
                 .foregroundColor(Color.green)
             Image("Surf Board")
+                .resizable()
+                .scaledToFit()
             Text("Order Pizza")
+                .font(.largeTitle)
             //.. spacers push everything away from the closest margin or object; since this is at bottom of vstack, it pushes whole thing up toward top
             Spacer()
         }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                //.. for dark mode
+                .colorScheme(.dark)
+                .background(Color.black)
+            //.. for specific device preview
+                .previewDevice("iPad Pro (9.7-inch)")
+                .previewDisplayName("iPad Pro 9.7 inch")
+            //.. to have multiple devices previewed, use Group and put another ContentView
+            ContentView()
+                .previewDevice("iPhone 12 Pro")
+                .previewDisplayName("Karen's iPhone 12 Pro")
+        }
     }
 }
