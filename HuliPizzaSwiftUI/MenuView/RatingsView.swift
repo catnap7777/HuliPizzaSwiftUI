a36754a3effef64071ebe06c139bb32ff7c26368
km <35552602+catnap7777@users.noreply.github.com>
2022-03-04 11:19:57 -0600

    working with dynamic collections

--- HuliPizzaSwiftUI/MenuView/RatingsView.swift
+++ HuliPizzaSwiftUI/MenuView/RatingsView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct RatingsView: View {
+    var count: Int = 5
+    var rating: [String] {
+        //.. all shape symbols have versions with a number in front of them
+        let symbolName = "\(count).circle"
+        return Array(repeating: symbolName, count: count)
+    }
     var body: some View {
+        //..must imbed in Hstack because it defaults to VStack
         HStack {
-            //..must imbed in Hstack because it defaults to VStack
-            ForEach(0..<4) { item in
+            ForEach(rating, id:\.self) { item in
                 //Image("Pizza Slice")
-                Image(systemName: "star.circle")
+                Image(systemName: item)
                     .font(.headline)
                     .foregroundColor(Color("G4"))
             }
