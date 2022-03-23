ef748d8d6ce398889227495d560709a82c402ebe
km <35552602+catnap7777@users.noreply.github.com>
2022-03-22 22:50:49 -0500

    finished lesson6

--- HuliPizzaSwiftUI/MenuView/MenuDetailView.swift
+++ HuliPizzaSwiftUI/MenuView/MenuDetailView.swift
@@ -1,126 +1,141 @@
 //
 //  MenuDetailView.swift
 //  Pizza
-//
-//  Created by Steven Lipton on 9/2/19.
+//  adaptive version for landscape.
+//  creates two functions of views to be able to switch them between Hstack and Vstack.
+//  Created by Steven Lipton on 12/26/20.
 //  Copyright © 2019 Steven Lipton. All rights reserved.
 //
 
 import SwiftUI
 ///A `View`for entering in an order. Takes basic information about the order from `menuItem`
+
+var size:Size = .medium
 struct MenuDetailView: View {
-   
-    @ObservedObject var orderModel: OrderModel
-    @EnvironmentObject var settings: UserPreferences
-    @State var didOrder: Bool = false
-    @State var quantity: Int = 1
     
+   @EnvironmentObject var settings:UserPreferences
+   @ObservedObject var orderModel:OrderModel
+    @State var didOrder:Bool = false
+    @State var quantity:Int = 1
     var menuItem:MenuItem
     var formattedPrice:String{
-        String(format:"%3.2f",menuItem.price * Double(quantity) * settings.size.rawValue)
+        String(format:"%3.2f",menuItem.price * Double(quantity) * size.rawValue)
     }
     func addItem(){
-        //.. don't need this here when presenting sheet; do need for just alert from below
 //        orderModel.add(menuID: menuItem.id)
         didOrder = true
     }
     
-    var body: some View {
-        VStack {
-            PageTitleView(title: menuItem.name)
-            SelectedImageView(image: "\(menuItem.id)_250w")
-                .padding(5)
-                .layoutPriority(3)
-            
-            Text(menuItem.description)
-                .lineLimit(5)
-                .padding()
-                .layoutPriority(3)
-                
-            Spacer()
-            Text("Pizza Size")
-                .fontWeight(.heavy)
-                .foregroundColor(Color.green)
-                .underline()
-                
-            //.. note: according to https://www.hackingwithswift.com/forums/swiftui/swiftui-xcode-12-2-picker-no-longer-shows-label/4809 Xcode 12 no longer shows picker label
-            SizePickerView(size: $settings.size)
-            QuantityStepperView(quantity: $quantity)
-            HStack{
-                Text("Order:  \(formattedPrice)")
-                    .font(.headline)
-                Spacer()
-                Text("Order total: " + orderModel.formattedTotal)
-                    .font(.headline)
-            }
-            .padding()
-            HStack{
-                Spacer()
-                Button(action: addItem) {
-                   Text("Add to order")
-                        .font(.title)
-                        .fontWeight(.bold)
+    //.. from video -> 6.4 -> takes the geometry proxy we get from geometry reader and looks at its height. If its height is greater than 414, which is about where you find most phones' width or height, depending on the smallest size of the phone, that's about the biggest point size right now. It's an arbitrary number. It really should be something that's a little more adaptive, but for this course, we're just going to leave it like that. I can now use this to say, if anything is smaller than that, I'm going to be considering it compact portrait.  I can now go down, back into my body code here, and we got that V stack over here. I'm going to put some if then else stuff here, and so I can say, if is compact portrait, geo, geo, I'll do one thing and then else I'll do another thing.
+    func isCompactPortrait(geo:GeometryProxy)->Bool{
+        return geo.size.height <= 414
+    }
+    
+    func titleView()->some View{
+         return
+            //.. GeometryReader to set proportions on phone
+            GeometryReader { geometry in
+                HStack{
+                    //.. so with frame below, this gives 3/5 for image and 2/5 for text in this hstack
+                    SelectedImageView(image: "\(self.menuItem.id)_250w")
+                        .padding(5)
+                    //.. so with frame below, this gives 3/5 for image and 2/5 for text in this hstack
+                    Text(self.menuItem.description)
                         .padding()
-                    .background(Color("G4"))
-                        .foregroundColor(Color("IP"))
-                        .cornerRadius(5)
+                        .frame(width: geometry.size.width * 2/5)
+                        .font(geometry.size.height > 200 ? .body : .caption)
+                    Spacer()
                 }
-//                .alert(isPresented: $didOrder) {
-//                    Alert(title: Text("Pizza Ordered"), message: Text("You ordered a " + self.menuItem.name))
-                .sheet(isPresented: $didOrder) {
-                    ConfirmView(menuID: self.menuItem.id, orderModel: self.orderModel, isPresented: self.$didOrder, quantity: $quantity, size: self.$settings.size)
-                }
-                Spacer()
             }
-            .padding(.top)
+                
+    }
+    
+    
+    
+    
+    func menuOptionsView()-> some View{
+       return  VStack{
+            SizePickerView(size:$settings.size)
+            QuantityStepperView(quantity:$quantity)
+            PageTitleView(title: "Order:  \(formattedPrice)")
             Spacer()
         }
-        
     }
-}
+        
+    
+    var body: some View {
+        //..GeometryReader to set for portrait vs landscape
+        GeometryReader { geo in
+            VStack {
+                HStack{
+                    PageTitleView(title: self.menuItem.name)
+                    Button(action: self.addItem) {
+                        Text("Add to order")
+                            //.. set the font depending on orientation - portrait vs landscape
+                            //.. Note: staticFont defined in PageTitleView.swift
+                            .font(isCompactPortrait(geo: geo) ? staticFont: .title)
+                            .fontWeight(.bold)
+                            .padding([.leading,.trailing])
+                            .background(Color("G3"))
+                            .foregroundColor(Color("IP"))
+                            .cornerRadius(5)
+                    }
+                    .sheet(isPresented: self.$didOrder){
+                        ConfirmView(menuID: self.menuItem.id, orderModel:self.orderModel, isPresented: self.$didOrder, quantity: self.$quantity, size:self.$settings.size)
+                    }
+                }
+                
+                //.. Figure out the orientation you want to use
+                //.. From video -> 6.4 -> Pretty much, I've just taken the title view and menu options view that I already had. And if it's portrait, I'm going to move them side by side. So if I have them in one situation, I'm going to have an Hstack and in the other situation, I'll have it a Vstack.
+                if isCompactPortrait(geo: geo) {
+                    HStack{
+                        self.titleView()
+                        self.menuOptionsView()
+                    }
+                } else {
+                    VStack{
+                        self.titleView()
+                        self.menuOptionsView()
+                    }
 
+                }
+                
+                
+            }//Root VStack
+            .padding(.top, 5)
+        }
+            
+    }// body
+}// MenuDetailView
 struct MenuDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuDetailView(orderModel: OrderModel(), menuItem: testMenuItem)
-            .environmentObject(UserPreferences())
+        MenuDetailView(orderModel:OrderModel(),menuItem: testMenuItem)
     }
 }
 
 struct QuantityStepperView: View {
-    @Binding var quantity: Int
+    @Binding var quantity:Int
     var body: some View {
-        Stepper(value: $quantity, in: 1...10) {
-            Text("Quantity: \(quantity)")
-                .bold()
+        Stepper(value: $quantity, in: 1...10){
+            Text("Quantity \(quantity)")
         }
-        
-        //            HStack{
-        //                Text("Quantity:")
-        //                Text("1")
-        //                    .bold()
-        //                Spacer()
-        //            }
-        .padding()
+            .padding()
     }
 }
 
+
 struct SizePickerView: View {
-    @Binding var size: Size
-    let sizes: [Size] = [.small, .medium, .large]
+    
+    @Binding var size:Size
+    let sizes:[Size] = [.small,.medium,.large]
     var body: some View {
-        Picker(selection: $size, label:Text("Pizza Size")) {
-            ForEach(sizes, id: \.self) { size in
+        Picker(selection:$size, label:Text("Pizza Size")){
+            ForEach(sizes, id:\.self){ size in
                 Text(size.formatted()).tag(size)
             }
         }
         .pickerStyle(SegmentedPickerStyle())
-        .background(Color("kamLightGreen"))
-        
-        //            HStack{
-        //                Spacer()
-        //                Text("Pizza size")
-        //                Text(settings.size.formatted())
-        //            }
         .font(.headline)
     }
 }
+
--- HuliPizzaSwiftUI/PageTitleView.swift
+++ HuliPizzaSwiftUI/PageTitleView.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 
+//.. global variable
+let staticFont = Font.system(size: 22)
+
 struct PageTitleView: View {
+    //.. to use system environment var to size fonts
+    //.. from video -> 6.5 -> So that'd be @Environment to get the wrapper. And environment variables that are coming from the system, you can actually use a key path to get them. So I'm going to put a parentheses here and then a ./ and this one's called vertical size class. And of course, though, I was looking for the horizontal size class, that'd be a horizontal size class. Now, once you put that in there, then you put var and then the name of it, which I'm going to use sizedClass here, like that. Now I've got another issue I'm going to watch out for because in these compact sizes, the dynamic type we've been using up to this point becomes an issue. You should use dynamic type whenever possible, allowing the user to control the font size. But sometimes that's going to mess with your layout. When it does, you going to want a static type.
+    @Environment(\.verticalSizeClass) var sizeClass
     var title: String
     var isDisplayingOrders: Bool! = nil
     var body: some View {
         HStack {
             Spacer()
             Text(title)
-                .font(.largeTitle)
+                //.. from video 6.5 @ 5:42 -> I don't have a problem with dynamic type when I'm in portrait, I only have a problem with it in landscape and I have now handled that situation. 
+                .font(sizeClass != .compact ? .largeTitle : staticFont)
                 .fontWeight(.heavy)
                 .padding(.trailing)
         } .overlay(
@@ -22,7 +29,8 @@ struct PageTitleView: View {
             Image(systemName: "chevron.up.square")
                 .rotationEffect(isDisplayingOrders ?? false ? Angle(degrees: 0.0) : Angle(degrees: 180.0))
                 .animation(.easeInOut(duration: 0.5))
-                .font(.title)
+                //.. from video 6.5 @ 5:42 -> I don't have a problem with dynamic type when I'm in portrait, I only have a problem with it in landscape and I have now handled that situation.
+                .font(sizeClass != .compact ? .title : staticFont)
                 .foregroundColor(isDisplayingOrders != nil ? Color("G1") : .clear)
                 .padding(),alignment: .leading
                 
